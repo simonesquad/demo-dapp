f3c05e2e4455b4bd6f08dd243aecfe6275356ab2
Simone Ballard <simoneballard@Simones-MacBook-Pro.local>
2024-06-03 15:30:33 -0500

    updated config which might work

--- chainlink-dapp-example/backend/hardhat.config.js
+++ chainlink-dapp-example/backend/hardhat.config.js
@@ -1,29 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-//require("@nomiclabs/hardhat-ethers")
- require('dotenv').config()
-
- const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || 
-"https://eth-sepolia.g.alchemy.com/v2/demo"
- const PRIVATE_KEY = process.env.PRIVATE_KEY || "-Jm83I6efKyL1Ux2NrxlFuoGFPmcXer7"
-
- module.exports = {
-     defaultNetwork: "sepolia",
-     networks: {
-         hardhat: {
-             // // If you want to do some forking, uncomment this
-             // forking: {
-             // url: MAINNET_RPC_URL
-             // }
-         },
-         localhost: {
-         },
-         sepolia: {
-             url: SEPOLIA_RPC_URL,
-             accounts: [PRIVATE_KEY],
-             saveDeployments: true,
-         },
-     },
-
-  solidity: "0.8.9",
-};
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/-Jm83I6efKyL1Ux2NrxlFuoGFPmcXer7",
+      accounts: []
+    }
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
